a3ef6e27e3a1a8b8804403e68e753ef14b51ed65
Henry Liu <hel044@ucsd.edu>
2025-11-15 10:21:00 -0800

    Add Cambridge route

--- map.js
+++ map.js
@@ -17,20 +17,35 @@ const map = new mapboxgl.Map({
   maxZoom: 18, // Maximum allowed zoom
 });
 
+const bikeLanePaint = {
+    'line-color': 'green',
+    'line-width': 3,
+    'line-opacity': 0.4,
+};
+
 map.on('load', async () => {
-    //code
+    // Sources
     map.addSource('boston_route', {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
     });
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data:'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    })
+
+    // Styling
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'bike-lanes-boston',
         type: 'line',
         source: 'boston_route',
-        paint: {
-          'line-color': 'green',
-          'line-width': 3,
-          'line-opacity': 0.4,
-        },
-      });
+        paint: bikeLanePaint
+    });
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeLanePaint
+    })
+
 });
